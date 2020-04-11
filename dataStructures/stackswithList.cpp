1e60c0590e74060c46b9cfff133bde5993179398
aniket_more <aniketavinashmore343@gmail.com>
2020-04-11 22:40:54 +0530

    added linked list and stack with a linked list

---
+++ dataStructures/stackswithList.cpp
@@ -0,0 +1,105 @@
+#include<iostream>
+using namespace std;
+
+template<typename T>
+class Lstack{
+    private:
+
+    class Node{
+        public:
+        T data;
+        Node *next;
+    };
+
+    Node *head = nullptr;
+    int size = 0;
+
+    public:
+    Lstack(){}
+
+    bool isempty(){
+        return(size==0);
+    }
+
+    int getSize(){
+        return size;
+    }
+
+    void push(T n){
+        if(isempty()){
+            Node *temp = new Node;
+            temp->data=n;
+            temp->next = nullptr;
+
+            head = temp;
+            size++;
+            return;
+        }
+        Node *temp = new Node;
+        temp->data = n;
+        temp->next = head;
+        
+        head =temp;
+        size++;
+        return;
+    }
+
+    T pop(){ //does not check underlflow
+        if(isempty()){
+            cerr<<"underflow error"<<endl;
+        }
+        Node *temp = head;
+
+        T element = head->data;
+        head = head->next;
+        delete temp;
+        size--;
+        return element;
+        
+
+    }
+
+    void display(){
+        Node *temp =head;
+        while(temp!=nullptr){
+            cout<<temp->data<<" ";
+            temp = temp->next;
+        }
+        cout<<"\n";
+    }
+
+    void clear(){
+        Node *trav = head;
+        Node *travNext = nullptr;
+
+        while(trav!=nullptr){
+            travNext = trav->next;
+            delete trav;
+            trav = travNext;
+        }
+
+        size =0;
+        head = nullptr;
+
+    }
+
+    ~Lstack(){
+        clear();
+    }
+
+
+
+};
+int main(){
+    Lstack<int> s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    s.display();
+
+    cout<<s.pop()<<" "<<s.pop()<<endl;
+
+    s.display();
+
+}
