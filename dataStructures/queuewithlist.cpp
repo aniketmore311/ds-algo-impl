d1f87389e8ea7ea4d79b0f058cea4d8d12ad3e24
aniket_more <aniketavinashmore343@gmail.com>
2020-04-17 17:25:13 +0530

    added clear method to queue

--- dataStructures/queuewithlist.cpp
+++ dataStructures/queuewithlist.cpp
@@ -1,61 +1,91 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Queue{
+class Queue
+{
 private:
-class Node{
+    class Node
+    {
     public:
         int value;
-        Node* next;
+        Node *next;
     };
-    
-    Node* head =nullptr;
+
+    Node *head = nullptr;
     Node *tail = nullptr;
-    int size=0;
+    int size = 0;
 
 public:
-    
-    Queue(){}//constructor
+    Queue() {} //constructor
+    ~Queue()
+    { //destructor
+        clear();
+    }
 
-    void Enqueue(int n){
-        Node* temp = new Node;
-        if(size==0){
-            temp->value=n;
-            temp->next =nullptr;
+    void Enqueue(int n)
+    {
+        Node *temp = new Node;
+        if (size == 0)
+        {
+            temp->value = n;
+            temp->next = nullptr;
             head = tail = temp;
             size++;
-        }else{
-            temp->value=n;
-            temp->next=nullptr;
-            tail->next=temp;
-            tail=temp;
+        }
+        else
+        {
+            temp->value = n;
+            temp->next = nullptr;
+            tail->next = temp;
+            tail = temp;
             size++;
         }
     }
 
-    int dequeue(){
-        if(size==0){
+    int dequeue()
+    {
+        if (size == 0)
+        {
             throw(std::underflow_error("empty list"));
-        }else{
-            Node* temp =head;
+        }
+        else
+        {
+            Node *temp = head;
             int ret = head->value;
-            head=head->next;
+            head = head->next;
             delete temp;
             size--;
             return ret;
         }
     }
 
-    void display(){
-        Node *temp =head;
-        while(temp!=nullptr){
-            cout<<temp->value<<" ";
+    void display()
+    {
+        Node *temp = head;
+        while (temp != nullptr)
+        {
+            cout << temp->value << " ";
             temp = temp->next;
         }
-        cout<<"\n";
+        cout << "\n";
     }
 
+    void clear()
+    {
+        Node *current = head;
+        Node *next = head;
+        while (current != nullptr)
+        {
+            next = current->next;
+            delete current;
+            current = next;
+        }
+        size = 0;
+        head = nullptr;
+        tail = nullptr;
+    }
 };
-int main(){
+int main()
+{
     Queue q;
     q.Enqueue(10);
     q.Enqueue(20);
