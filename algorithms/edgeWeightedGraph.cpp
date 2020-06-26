5f35093b69600e310d26e5feb5b9551dc0e4d95d
aniket_more <aniketavinashmore343@gmail.com>
2020-06-26 21:22:19 +0530

    added a weighted graph class
    the class is similar to the normal graph but uses pairs instead in ints
    to represent edges

---
+++ algorithms/edgeWeightedGraph.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(x) cout << #x << ": " << x << endl
+
+class EdgeWeightedGraph
+{
+private:
+  vector<vector<pair<int, double>>> adj; // a vector(adj) of vectors(adjecency list) of pairs(weighted edge) of int(end vertex) and doubles(edge weight)
+  int vertices;
+  int edges;
+
+public:
+  EdgeWeightedGraph(int vertices)
+  {
+    adj.resize(vertices);
+    this->vertices = vertices;
+    // debug(adj.size());
+  }
+
+  void addEdge(int u, int v, double weight)
+  {
+    adj.at(u).push_back({v, weight});
+  }
+
+  void undirectedInput(int edges)
+  {
+    this->edges = edges;
+    int u, v;
+    double inputWeight;
+    for (int i = 0; i < edges; i++)
+    {
+      std::cin >> u >> v >> inputWeight;
+
+      addEdge(u, v, inputWeight);
+      addEdge(v, u, inputWeight);
+    }
+  }
+
+  void printGraph()
+  {
+    int i = 0;
+    for (auto list : adj)
+    {
+      std::cout << i++ << " -> ";
+      for (auto elementPair : list)
+      {
+        cout << " (" << elementPair.first << ", " << elementPair.second << "), ";
+      }
+      std::cout << std::endl;
+    }
+  }
+};
+
+int main()
+{
+  freopen("builds/input.txt", "r", stdin);
+  freopen("builds/output.txt", "w", stdout);
+
+  EdgeWeightedGraph g(4);
+  g.undirectedInput(4);
+  g.printGraph();
+}
