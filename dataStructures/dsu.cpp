463a6922d61b7d83becf3910bb823ad65fae8b97
aniket_more <aniketavinashmore343@gmail.com>
2020-06-29 12:11:54 +0530

    made dsu into a class

--- dataStructures/dsu.cpp
+++ dataStructures/dsu.cpp
@@ -5,80 +5,71 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int parent[100];
-int size[100];
-
-// iterative find function
-int find(int x)
+class dsu
 {
-  // follow the links upwards until you find a self linked node
-  // that node is the parent
-  while (parent[x] != x)
-    x = parent[x];
-  return x;
-}
+public:
+  vector<int> parent;
+  vector<int> size;
 
-// a recursive find function that uses path compression
-int findRecursive(int x)
-{
-  if (parent[x] == x)
+  dsu(int n)
+  {
+    // making all the elements parent of themselves
+    for (int i = 0; i < n; i++)
+    {
+      parent.push_back(i);
+    }
+
+    // making all the sizes 1
+    size.resize(n, 1);
+  }
+
+  // iterative find function
+  int find(int x)
+  {
+    // follow the links upwards until you find a self linked node
+    // that node is the parent
+    while (parent[x] != x)
+      x = parent[x];
     return x;
+  }
 
-  int result = findRecursive(parent[x]);
-  parent[x] = result; // applying path compression
-  return result;
-}
+  // a recursive find function that uses path compression
+  int findRecursive(int x)
+  {
+    if (parent[x] == x)
+      return x;
 
-bool same(int x, int y)
-{
-  return (find(x) == find(y));
-}
+    int result = findRecursive(parent[x]);
+    parent[x] = result; // applying path compression
+    return result;
+  }
 
-void unite(int a, int b)
-{
-  a = find(a);
-  b = find(b);
-  // now a and b are the parents of the original arguments
+  bool same(int x, int y)
+  {
+    return (find(x) == find(y));
+  }
 
-  if (a == b)
-    return; // return if already in the same group
+  void unite(int a, int b)
+  {
+    a = find(a);
+    b = find(b);
+    // now a and b are the parents of the original arguments
 
-  if (size[a] < size[b])
-    swap(a, b);
-  // now a is the big set's parent and b is the small set's parent
-  size[a] += size[b];
-  // the size of set a is increased to accomodate b
-  parent[b] = a;
-  // b is linkded to a
-}
+    if (a == b)
+      return; // return if already in the same group
+
+    if (size[a] < size[b])
+      swap(a, b);
+    // now a is the bigger set's parent and b is the smaller set's parent
+    size[a] += size[b];
+    // the size of set a is increased to accomodate b
+    parent[b] = a;
+    // b is linkded to a
+  }
+};
 
 int main()
 {
   freopen("builds/input.txt", "r", stdin);
   freopen("builds/output.txt", "w", stdout);
-
-  // int i;
-  // for (i = 0; i < 100; i++)
-  // {
-  //   parent[i] = i;
-  //   size[i] = 1;
-  // }
-
-  // int e = 5;
-  // for (i = 0; i < e; i++)
-  // {
-  //   int a, b;
-  //   cin >> a >> b;
-  //   unite(a, b);
-  // }
-
-  // for (i = 1; i < 6; i++)
-  // {
-  //   cout << findRecursive(i) << endl;
-  // }
-
-  // for (i = 1; i < 6; i++)
-  // {
-  //   cout << parent[i] << endl;
-  // }
 }
