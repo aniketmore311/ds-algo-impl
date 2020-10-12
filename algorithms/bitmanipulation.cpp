0ed9537f3bcd7008ac20fc200d591ce21eb93a54
aniket more <aniketavinashmore343@gamil.com>
2020-10-12 19:19:25 +0530

    modify bit manipulation

--- algorithms/bitmanipulation.cpp
+++ algorithms/bitmanipulation.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+
 int countBits(unsigned int n)
 {
     int count = 0;
@@ -10,6 +11,30 @@ int countBits(unsigned int n)
     }
     return count;
 }
+
+int setBits(int n)
+{
+    int count{0};
+    // while any bit is remaining
+    while (n > 0)
+    {
+        // increse the count
+        count++;
+        // remove that set bit
+        n = n & (n - 1);
+    }
+    return count;
+}
+
+bool isPowerof2(int n)
+{
+    if (n == 0)
+        return false;
+    while (n % 2 == 0)
+        n = n >> 1;
+    return (n == 1);
+}
+
 int main()
 {
     for (int i = 0; i <= 8; i++)
